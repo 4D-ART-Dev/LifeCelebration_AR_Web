2a6b735818f19b8a1d47f528c669e2bc08f1bca4
jwkim9612 <jwkim9612@gmail.com>
2026-04-08 10:28:07 +0900

    t

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "FourDArt-LifeCelebration_AR-0.1.6";
+const cacheName = "FourDArt-LifeCelebration_AR-0.1.7";
 const contentToCache = [
     "Build/LifeCelebration_AR.loader.js",
     "Build/LifeCelebration_AR.framework.js.unityweb",
